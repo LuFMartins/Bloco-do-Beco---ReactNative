3a60054583b254e262e0234938f85ee8a65fa5a7
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2025-11-16 19:09:19 -0300

    Testando e organizando o ambiente

---
+++ src/app/screens/home/index.jsx
@@ -0,0 +1,11 @@
+import {View, Text, ScrollView, TouchableOpacity} from "react-native";
+
+export default function Index(){
+    return(
+        <View>
+            <Text>Olá, Mundo</Text>
+            <TouchableOpacity>Olá, Mundo</TouchableOpacity>
+        </View>
+    )
+}
+
